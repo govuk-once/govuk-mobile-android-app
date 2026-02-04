9c89109ee6fd0a54fa36cd4c688b60728fdec52c
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2026-02-04 13:07:12 +0000

    Update loading animation image - Remove animation from ellipses - No fallback when animations turned off

--- feature/chat/src/main/kotlin/uk/gov/govuk/chat/ui/component/ChatEntry.kt
+++ feature/chat/src/main/kotlin/uk/gov/govuk/chat/ui/component/ChatEntry.kt
@@ -1,8 +1,5 @@
 package uk.gov.govuk.chat.ui.component
 
-import android.graphics.ImageDecoder
-import android.graphics.drawable.AnimatedImageDrawable
-import android.widget.ImageView
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
@@ -15,26 +12,30 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.LiveRegionMode
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.liveRegion
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import kotlinx.coroutines.delay
 import uk.gov.govuk.chat.R
 import uk.gov.govuk.chat.ui.model.ChatEntry
 import uk.gov.govuk.design.ui.component.BodyRegularLabel
 import uk.gov.govuk.design.ui.component.MediumVerticalSpacer
 import uk.gov.govuk.design.ui.component.SmallHorizontalSpacer
+import uk.gov.govuk.design.ui.extension.areAnimationsEnabled
 
 @Composable
 internal fun ChatEntry(
@@ -161,33 +162,28 @@ private fun Loading(
         modifier = modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        AndroidView(
-            factory = {
-                ImageView(it).apply {
-                    val source = ImageDecoder.createSource(context.resources, R.drawable.ic_generating_answer)
-                    val drawable = ImageDecoder.decodeDrawable(source)
-                    setImageDrawable(drawable)
-                    if (drawable is AnimatedImageDrawable) {
-                        drawable.start()
-                    }
-                }
-            },
-            modifier = Modifier.size(24.dp)
-        )
+        if (LocalContext.current.areAnimationsEnabled()) {
+            val composition by rememberLottieComposition(
+                LottieCompositionSpec.RawRes(R.raw.generating_answer)
+            )
 
-        SmallHorizontalSpacer()
+            val progress by animateLottieCompositionAsState(
+                composition = composition,
+                iterations = LottieConstants.IterateForever,
+                isPlaying = LocalContext.current.areAnimationsEnabled()
+            )
 
-        var dots by remember { mutableIntStateOf(0) }
+            LottieAnimation(
+                composition = composition,
+                progress = { progress },
+                modifier = Modifier.size(24.dp)
+            )
 
-        LaunchedEffect(Unit) {
-            while (true) {
-                delay(1200L)
-                dots = (dots + 1) % 4 // 0,1,2,3
-            }
+            SmallHorizontalSpacer()
         }
 
         BodyRegularLabel(
-            text = stringResource(R.string.loading_text) + ".".repeat(dots),
+            text = stringResource(R.string.loading_text),
             modifier = Modifier.padding(top = 2.dp)
         )
     }
