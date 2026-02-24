1ac3b02433c7908912fe9f385dfdccff86325a50
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2026-02-24 12:59:12 +0000

    Review suggestion - introduce new Label component and use it for the existing error screen and the new onboarding screen 3 - remove Cancel link as not required

--- feature/chat/src/main/kotlin/uk/gov/govuk/chat/ui/OnboardingPageThree.kt
+++ feature/chat/src/main/kotlin/uk/gov/govuk/chat/ui/OnboardingPageThree.kt
@@ -2,19 +2,13 @@ package uk.gov.govuk.chat.ui
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import uk.gov.govuk.chat.BuildConfig
@@ -22,11 +16,11 @@ import uk.gov.govuk.chat.ChatViewModel
 import uk.gov.govuk.chat.R
 import uk.gov.govuk.chat.domain.Analytics
 import uk.gov.govuk.design.ui.component.BodyRegularLabel
+import uk.gov.govuk.design.ui.component.BodyRegularLabelCenteredTrailingLink
 import uk.gov.govuk.design.ui.component.FullScreenHeader
 import uk.gov.govuk.design.ui.component.MediumVerticalSpacer
 import uk.gov.govuk.design.ui.component.PrimaryButton
 import uk.gov.govuk.design.ui.component.SecondaryButton
-import uk.gov.govuk.design.ui.model.HeaderActionStyle
 import uk.gov.govuk.design.ui.model.HeaderDismissStyle
 import uk.gov.govuk.design.ui.theme.GovUkTheme
 
@@ -91,11 +85,7 @@ private fun OnboardingPageThreeScreen(
         title = stringResource(id = R.string.onboarding_page_three_header),
         headerContent = {
             FullScreenHeader(
-                dismissStyle = HeaderDismissStyle.Back(onBack),
-                actionStyle = HeaderActionStyle.ActionButton(
-                    title = stringResource(id = R.string.onboarding_page_cancel_text),
-                    onClick = onCancel
-                )
+                dismissStyle = HeaderDismissStyle.Back(onBack)
             )
         },
         screenContent = {
@@ -106,7 +96,21 @@ private fun OnboardingPageThreeScreen(
             )
 
             MediumVerticalSpacer()
-            ContentWithLink()
+            val introText = stringResource(id = R.string.onboarding_page_three_text_two)
+            val outroText = "."
+            val linkText = stringResource(id = R.string.onboarding_page_three_text_two_link_text)
+            val altText = "$introText $linkText ${stringResource(R.string.sources_open_in_text)}$outroText"
+            val uriHandler = LocalUriHandler.current
+            val url = BuildConfig.PRIVACY_POLICY_URL
+
+            BodyRegularLabelCenteredTrailingLink(
+                introText = introText,
+                outroText = outroText,
+                linkText = linkText,
+                onClick = { uriHandler.openUri(url) },
+                altText = altText,
+                textColor = GovUkTheme.colourScheme.textAndIcons.primary
+            )
         },
         buttonContent = {
             PrimaryButton(
@@ -121,56 +125,16 @@ private fun OnboardingPageThreeScreen(
             SecondaryButton(
                 text = stringResource(id = R.string.onboarding_page_three_decline_button),
                 onClick = onCancel,
-                modifier = Modifier.padding(top = GovUkTheme.spacing.medium)
+                modifier = Modifier
+                    .padding(
+                        start = GovUkTheme.spacing.medium,
+                        end = GovUkTheme.spacing.medium,
+                        top = GovUkTheme.spacing.medium
+                    )
             )
         },
         modifier = modifier,
         animationRes = R.raw.chat_onboarding_three
-    )
-}
-
-@Composable
-private fun ContentWithLink(
-    modifier: Modifier = Modifier
-) {
-    val intro = stringResource(id = R.string.onboarding_page_three_text_two)
-    val linkText = stringResource(id = R.string.onboarding_page_three_text_two_link_text)
-    val outro = "."
-    val url = BuildConfig.PRIVACY_POLICY_URL
-
-    val uriHandler = LocalUriHandler.current
-
-    val annotatedString = buildAnnotatedString {
-        append(intro)
-        append(" ")
-        pushStringAnnotation(tag = "URL", annotation = url)
-        withStyle(
-            style = SpanStyle(
-                color = GovUkTheme.colourScheme.textAndIcons.link
-            )
-        ) {
-            append(linkText)
-        }
-        pop()
-        append(outro)
-    }
-
-    Text(
-        text = annotatedString,
-        style = GovUkTheme.typography.bodyRegular.copy(
-            color = GovUkTheme.colourScheme.textAndIcons.primary,
-            textAlign = TextAlign.Center
-        ),
-        modifier = modifier
-            .fillMaxWidth()
-            .clickable {
-                annotatedString
-                    .getStringAnnotations(tag = "URL", start = 0, end = annotatedString.length)
-                    .firstOrNull()
-                    ?.let { annotation ->
-                        uriHandler.openUri(annotation.item)
-                    }
-            }
     )
 }
 
