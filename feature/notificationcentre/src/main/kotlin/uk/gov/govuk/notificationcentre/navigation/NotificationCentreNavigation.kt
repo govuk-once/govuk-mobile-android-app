ed5af680112240ba18ee32661cbd7588a4e80443
Matt Malone <matthew.malone@digital.cabinet-office.gov.uk>
2026-03-04 17:28:04 +0000

    feat: NOT-143 Notification Centre List

--- feature/notificationcentre/src/main/kotlin/uk/gov/govuk/notificationcentre/navigation/NotificationCentreNavigation.kt
+++ feature/notificationcentre/src/main/kotlin/uk/gov/govuk/notificationcentre/navigation/NotificationCentreNavigation.kt
@@ -3,17 +3,25 @@ package uk.gov.govuk.notificationcentre.navigation
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import androidx.navigation.navigation
+import uk.gov.govuk.notificationcentre.Notification
+import uk.gov.govuk.notificationcentre.ui.NotificationCentreDetailRoute
 import uk.gov.govuk.notificationcentre.ui.NotificationCentreRoute
 
 const val NOTIFICATION_CENTRE_GRAPH_ROUTE = "notification_centre_graph_route"
 private const val NOTIFICATION_CENTRE_ROUTE = "notification_centre_route"
+private const val NOTIFICATION_CENTRE_DETAIL_ROUTE = "notification_centre_detail_route"
+
+const val NOTIFICATION_CENTRE_DETAIL_ID_ARG = "notificationID"
 const val NOTIFICATION_CENTRE_GRAPH_START_DESTINATION = NOTIFICATION_CENTRE_ROUTE
 
 val notificationCentreDeepLinks = mapOf("/notification" to listOf(NOTIFICATION_CENTRE_ROUTE))
 
 fun NavGraphBuilder.notificationCentreGraph(
+    navController: NavController,
     modifier: Modifier
 ) {
 
@@ -24,11 +32,28 @@ fun NavGraphBuilder.notificationCentreGraph(
         composable(
             NOTIFICATION_CENTRE_ROUTE
         ) {
-            NotificationCentreRoute(modifier)
+            NotificationCentreRoute(modifier, onBack = {
+              navController.popBackStack()
+            }, onTapNotification = {
+                navController.navigateToNotificationCentreDetail(it)
+            })
+        }
+
+        composable("$NOTIFICATION_CENTRE_DETAIL_ROUTE/{$NOTIFICATION_CENTRE_DETAIL_ID_ARG}",
+            arguments = listOf(
+                navArgument(NOTIFICATION_CENTRE_DETAIL_ID_ARG) { type = NavType.StringType },
+            )) {
+            NotificationCentreDetailRoute(modifier, onBack = {
+                navController.popBackStack()
+            })
         }
     }
 }
 
 fun NavController.navigateToNotificationCentre() {
     navigate(NOTIFICATION_CENTRE_ROUTE)
+}
+
+fun NavController.navigateToNotificationCentreDetail(notification: Notification) {
+    navigate("$NOTIFICATION_CENTRE_DETAIL_ROUTE/$notification.id")
 }
