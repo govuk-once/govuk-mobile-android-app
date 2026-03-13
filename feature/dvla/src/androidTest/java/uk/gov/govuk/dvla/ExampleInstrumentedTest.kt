439e0cbeb2af47c23def7134a31648a4d32823dd
Lampros.Gkotsoulias <lampros.gkotsoulias@digital.cabinet-office.gov.uk>
2026-03-13 11:40:29 +0000

    Package name fixed

--- feature/dvla/src/androidTest/java/uk/gov/govuk/dvla/ExampleInstrumentedTest.kt
+++ feature/dvla/src/androidTest/java/uk/gov/govuk/dvla/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("uk.gov.govuk.data.test", appContext.packageName)
+        assertEquals("uk.gov.govuk.dvla.test", appContext.packageName)
     }
 }
