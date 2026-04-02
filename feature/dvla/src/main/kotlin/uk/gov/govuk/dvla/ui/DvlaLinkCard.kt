18d568551ada706932c8611efeb4cb14b99cb7e4
Lampros.Gkotsoulias <lampros.gkotsoulias@digital.cabinet-office.gov.uk>
2026-04-02 16:29:44 +0100

    Showing spinner when checking dvla status

--- feature/dvla/src/main/kotlin/uk/gov/govuk/dvla/ui/DvlaLinkCard.kt
+++ feature/dvla/src/main/kotlin/uk/gov/govuk/dvla/ui/DvlaLinkCard.kt
@@ -1,10 +1,15 @@
 package uk.gov.govuk.dvla.ui
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import uk.gov.govuk.design.ui.component.DrillInCard
+import uk.gov.govuk.design.ui.theme.GovUkTheme
 import uk.gov.govuk.dvla.DvlaLinkState
 import uk.gov.govuk.dvla.R
 
@@ -20,12 +25,22 @@ internal fun DvlaLinkCard(
         DvlaLinkState.CHECKING -> null
     }
 
-    // only show card when state known
     if (titleResId != null) {
         DrillInCard(
             title = stringResource(titleResId),
             onClick = onActionClick,
             modifier = modifier.fillMaxWidth()
         )
+    } else {
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(vertical = GovUkTheme.spacing.medium),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(
+                color = GovUkTheme.colourScheme.surfaces.primary
+            )
+        }
     }
 }
