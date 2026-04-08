ebd4d41548d35117b189e1ef809da2abf9cb17d3
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2026-04-08 13:57:56 +0100

    Fix Sonar issue - Make this interface functional or replace it with a function type

--- config/src/main/kotlin/uk/gov/govuk/config/data/remote/ContentApi.kt
+++ config/src/main/kotlin/uk/gov/govuk/config/data/remote/ContentApi.kt
@@ -3,7 +3,7 @@ package uk.gov.govuk.config.data.remote
 import retrofit2.Response
 import retrofit2.http.GET
 
-interface ContentApi {
+fun interface ContentApi {
     @GET("content/guidance/govuk-app-terms-and-conditions")
     suspend fun getContent(): Response<String>
 }
