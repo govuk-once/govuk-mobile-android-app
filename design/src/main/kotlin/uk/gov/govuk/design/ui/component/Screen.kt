c4e9aae565be8dffa3f160d5731972fc000badf3
Lampros.Gkotsoulias <lampros.gkotsoulias@digital.cabinet-office.gov.uk>
2026-04-14 15:23:34 +0100

    A11y and ui tweaks

--- design/src/main/kotlin/uk/gov/govuk/design/ui/component/Screen.kt
+++ design/src/main/kotlin/uk/gov/govuk/design/ui/component/Screen.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import uk.gov.govuk.design.R
@@ -102,8 +103,8 @@ fun BookendToWebScreen(
         IconButton(
             onClick = onClose,
             modifier = Modifier.padding(
-                top = GovUkTheme.spacing.medium,
-                start = GovUkTheme.spacing.small
+                top = GovUkTheme.spacing.small,
+                start = GovUkTheme.spacing.extraSmall
             )
         ) {
             Icon(
@@ -125,7 +126,8 @@ fun BookendToWebScreen(
         ) {
             LargeTitleBoldLabel(
                 text = title,
-                color = GovUkTheme.colourScheme.textAndIcons.primaryInverse
+                color = GovUkTheme.colourScheme.textAndIcons.primaryInverse,
+                modifier = Modifier.semantics { heading() }
             )
 
             MediumVerticalSpacer()
