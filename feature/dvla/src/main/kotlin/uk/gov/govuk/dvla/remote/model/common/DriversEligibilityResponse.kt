c36c06e1a0ef42c139ccabefe4f025aa5f6f4a42
Lampros.Gkotsoulias <lampros.gkotsoulias@digital.cabinet-office.gov.uk>
2026-04-17 10:50:14 +0100

    Removed renamed classes

--- feature/dvla/src/main/kotlin/uk/gov/govuk/dvla/remote/model/common/DriversEligibilityResponse.kt
+++
@@ -1,7 +0,0 @@
-package uk.gov.govuk.dvla.remote.model.common
-
-import com.google.gson.annotations.SerializedName
-
-data class DriversEligibilityResponse(
-    @SerializedName("applications") val applications: List<Application>
-)
