7b568e675fb88b8bf18b07bd007fd3414a969274
Lampros.Gkotsoulias <lampros.gkotsoulias@digital.cabinet-office.gov.uk>
2026-05-01 17:04:45 +0100

    Showing alert dialog

--- feature/settings/src/main/kotlin/uk/gov/govuk/settings/ui/YourAccountsScreen.kt
+++ feature/settings/src/main/kotlin/uk/gov/govuk/settings/ui/YourAccountsScreen.kt
@@ -6,11 +6,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import uk.gov.govuk.design.ui.component.BodyBoldLabel
+import uk.gov.govuk.design.ui.component.BodyRegularLabel
 import uk.gov.govuk.design.ui.component.ChildPageHeader
 import uk.gov.govuk.design.ui.component.InternalLinkListItem
 import uk.gov.govuk.design.ui.component.LargeVerticalSpacer
@@ -42,6 +51,8 @@ private fun YourAccountsScreen(
     onBack: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+
+    var accountToUnlink by remember { mutableStateOf<LinkedAccountUiModel?>(null) }
 
     Column(
         modifier
@@ -79,15 +90,73 @@ private fun YourAccountsScreen(
                                 icon = uk.gov.govuk.design.R.drawable.ic_cancel_round,
                                 altText = stringResource(R.string.your_accounts_remove_account_alt_text, displayTitle)
                             ) {
-                                // TODO in future ticket: accountToUnlink = account
-                                account.onUnlink()
+                                accountToUnlink = account
                             }
                         )
                     }
                 }
             }
         }
+
+        accountToUnlink?.let { account ->
+            RemoveAccountDialog(
+                displayTitle = stringResource(id = account.displayTitleRes),
+                onConfirm = {
+                    account.onUnlink()
+                    accountToUnlink = null
+                },
+                onDismiss = {
+                    accountToUnlink = null
+                }
+            )
+        }
     }
+}
+
+@Composable
+private fun RemoveAccountDialog(
+    displayTitle: String,
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        shape = RoundedCornerShape(GovUkTheme.numbers.cornerAndroidList),
+        modifier = modifier,
+        title = {
+            BodyBoldLabel(
+                text = "Remove $displayTitle?"
+            )
+        },
+        text = {
+            BodyRegularLabel(
+                text = "You will not be able to see information from this account in the app, unless you add it again. You can still see it on the GOV.UK website.",
+                color = GovUkTheme.colourScheme.textAndIcons.secondary
+            )
+        },
+        confirmButton = {
+            TextButton(
+                onClick = onConfirm
+            ) {
+                BodyBoldLabel(
+                    text = "Remove account",
+                    color = GovUkTheme.colourScheme.textAndIcons.buttonDestructive
+                )
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = onDismiss
+            ) {
+                BodyRegularLabel(
+                    text = "Cancel",
+                    color = GovUkTheme.colourScheme.textAndIcons.linkSecondary
+                )
+            }
+        },
+        containerColor = GovUkTheme.colourScheme.surfaces.alert
+    )
 }
 
 @Preview(showBackground = true)
