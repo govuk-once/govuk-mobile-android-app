d5ef320bfb7e0f5f91011192d3570eda2c805fb6
David Carr <david.carr@digital.cabinet-office.gov.uk>
2026-05-11 10:23:56 +0100

    GOVUKAPP-3491: Local explainer screen (#702)
    Hide icon in landscape

--- feature/local/src/main/kotlin/uk/govuk/app/local/ui/LocalExplainerScreen.kt
+++ feature/local/src/main/kotlin/uk/govuk/app/local/ui/LocalExplainerScreen.kt
@@ -1,32 +1,26 @@
 package uk.govuk.app.local.ui
 
+import android.R.attr.text
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.heading
-import androidx.compose.ui.semantics.isTraversalGroup
 import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.semantics.traversalIndex
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import uk.gov.govuk.design.ui.component.BodyRegularLabel
+import uk.gov.govuk.design.ui.component.CentreAlignedScreen
 import uk.gov.govuk.design.ui.component.FixedPrimaryButton
 import uk.gov.govuk.design.ui.component.LargeTitleBoldLabel
-import uk.gov.govuk.design.ui.component.RunOnceLaunchedEffect
 import uk.gov.govuk.design.ui.component.MediumVerticalSpacer
+import uk.gov.govuk.design.ui.component.RunOnceLaunchedEffect
 import uk.gov.govuk.design.ui.component.SmallVerticalSpacer
 import uk.gov.govuk.design.ui.theme.GovUkTheme
 import uk.govuk.app.local.LocalExplainerViewModel
@@ -62,68 +56,44 @@ private fun LocalExplainerScreen(
         onPageView()
     }
 
-    Scaffold(
-        containerColor = GovUkTheme.colourScheme.surfaces.background,
-        modifier = modifier.fillMaxWidth(),
+    Column(modifier) {
+        LocalFullScreenHeader(
+            onBack = onBack,
+        )
+        CentreAlignedScreen(
+            screenContent = {
+                val shouldShowIcon =
+                    LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT
 
-        topBar = {
-            LocalFullScreenHeader(
-                onBack = onBack,
-                modifier = Modifier.padding(bottom = GovUkTheme.spacing.large)
-            )
-        },
-        bottomBar = {
-            BottomNavBar(
-                onContinueClick = onContinueClick,
-                modifier = Modifier
-                    .semantics {
-                        isTraversalGroup = true
-                        traversalIndex = 1f
-                    }
-            )
-        }
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxHeight()
-                .verticalScroll(rememberScrollState()),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            SmallVerticalSpacer()
-            Image(
-                painter = painterResource(id = R.drawable.local_icon),
-                contentDescription = null
-            )
-            MediumVerticalSpacer()
-            LargeTitleBoldLabel(
-                text = stringResource(R.string.local_title),
-                color = GovUkTheme.colourScheme.textAndIcons.primary,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.semantics { heading() }
-            )
-            SmallVerticalSpacer()
-            BodyRegularLabel(
-                text = stringResource(R.string.local_explainer),
-                color = GovUkTheme.colourScheme.textAndIcons.primary,
-                modifier = Modifier.padding(horizontal = GovUkTheme.spacing.extraLarge),
-                textAlign = TextAlign.Center
-            )
-        }
-    }
-}
-
-@Composable
-private fun BottomNavBar(
-    onContinueClick: (String) -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Column(modifier = modifier.background(GovUkTheme.colourScheme.surfaces.background)) {
-        val buttonText = stringResource(R.string.local_continue_button)
-        FixedPrimaryButton(
-            text = buttonText,
-            onClick = { onContinueClick(buttonText) },
+                if (shouldShowIcon) {
+                    SmallVerticalSpacer()
+                    Image(
+                        painter = painterResource(id = R.drawable.local_icon),
+                        contentDescription = null
+                    )
+                    MediumVerticalSpacer()
+                }
+                LargeTitleBoldLabel(
+                    text = stringResource(R.string.local_title),
+                    color = GovUkTheme.colourScheme.textAndIcons.primary,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.semantics { heading() }
+                )
+                SmallVerticalSpacer()
+                BodyRegularLabel(
+                    text = stringResource(R.string.local_explainer),
+                    color = GovUkTheme.colourScheme.textAndIcons.primary,
+                    modifier = Modifier.padding(horizontal = GovUkTheme.spacing.extraLarge),
+                    textAlign = TextAlign.Center
+                )
+            },
+            footerContent = {
+                val buttonText = stringResource(R.string.local_continue_button)
+                FixedPrimaryButton(
+                    text = buttonText,
+                    onClick = { onContinueClick(buttonText) },
+                )
+            }
         )
     }
 }
