49f62d433650d9aeb9dabff0472ec24bf87d370f
Lampros.Gkotsoulias <lampros.gkotsoulias@digital.cabinet-office.gov.uk>
2026-05-18 14:20:48 +0100

    Handling list of vehicles

--- feature/dvla/src/main/kotlin/uk/gov/govuk/dvla/ui/VehicleAndLicenceSummaryWidget.kt
+++ feature/dvla/src/main/kotlin/uk/gov/govuk/dvla/ui/VehicleAndLicenceSummaryWidget.kt
@@ -1,12 +1,16 @@
 package uk.gov.govuk.dvla.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import uk.gov.govuk.design.ui.component.LoaderCard
+import uk.gov.govuk.design.ui.theme.GovUkTheme
 import uk.gov.govuk.dvla.VehicleAndLicenceSummaryUiState
 import uk.gov.govuk.dvla.VehicleAndLicenceSummaryViewModel
 import uk.gov.govuk.dvla.ui.component.VehicleSummaryCard
@@ -30,16 +34,22 @@ fun VehicleAndLicenceSummaryWidget(
             // TODO placeholder for now, tbc in future tickets
         }
         is VehicleAndLicenceSummaryUiState.Success -> {
-            // TODO support list of vehicles
-            val vehicle = currentState.vehicles.firstOrNull()
-
-            if (vehicle != null) {
-                VehicleSummaryCard(
-                    vehicleSummary = vehicle,
-                    onDetailsClick = {  },
-                    onMoreClick = {  },
-                    modifier = modifier.fillMaxWidth()
-                )
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                currentState.vehicles.forEach { vehicle ->
+                    VehicleSummaryCard(
+                        vehicleSummary = vehicle,
+                        onDetailsClick = {
+                            // TODO to be handled in next ticket(s)
+                        },
+                        onMoreClick = {
+                            // TODO to be handled in next ticket(s)
+                        },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
             }
         }
     }
