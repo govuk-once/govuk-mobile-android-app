005f62346a78c649a56066c71c8064fd0acb302e
TimDenyer <timgjdenyer@gmail.com>
2026-06-16 17:30:13 +0100

    Change functions to internal

--- feature/dvla/src/main/kotlin/uk/gov/govuk/dvla/util/SpecificationsFormatter.kt
+++ feature/dvla/src/main/kotlin/uk/gov/govuk/dvla/util/SpecificationsFormatter.kt
@@ -1,21 +1,20 @@
 package uk.gov.govuk.dvla.util
 
-fun getFormattedEngineCapacity(engineCapacity: Int): String {
+internal fun getFormattedEngineCapacity(engineCapacity: Int): String {
     if (engineCapacity < 1000) return "${engineCapacity}cc"
     val capacityInLitres = "${engineCapacity / 100 / 10.0}L"
     return capacityInLitres
 }
 
-fun getFormattedVehicleColour(
+internal fun getFormattedVehicleColour(
     colour: String,
     concatenator: String,
     secondaryColour: String
-): String {
-    return "$colour $concatenator $secondaryColour"
-}
+) = "$colour $concatenator $secondaryColour"
 
-fun getFormattedEngineCapacityAltText(engineCapacity: String, replacementText: String) =
+
+internal fun getFormattedEngineCapacityAltText(engineCapacity: String, replacementText: String) =
     engineCapacity.replace("L", " $replacementText")
 
-fun getFormattedEmissionsAltText(euroStatus: String, replacementText: String) =
+internal fun getFormattedEmissionsAltText(euroStatus: String, replacementText: String) =
     euroStatus.replace("g/km", " $replacementText")
