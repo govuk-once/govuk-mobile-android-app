70d0abc9e8fe208044d3b2b540160510a3df5b1f
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2026-06-18 08:54:39 +0100

    Add SDK theme support

--- analytics/src/main/kotlin/uk/gov/govuk/analytics/QualtricsAnalyticsClient.kt
+++ analytics/src/main/kotlin/uk/gov/govuk/analytics/QualtricsAnalyticsClient.kt
@@ -3,6 +3,22 @@ package uk.gov.govuk.analytics
 import android.content.Context
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.qualtrics.digital.Qualtrics
+import com.qualtrics.digital.QualtricsTheme
+import com.qualtrics.digital.theming.embedded.EmbeddedAppFeedbackTheme
+import com.qualtrics.digital.theming.embedded.FollowupQuestionTheme
+import com.qualtrics.digital.theming.embedded.InitialQuestionTheme
+import com.qualtrics.digital.theming.embedded.MultipleChoiceTheme
+import com.qualtrics.digital.theming.embedded.RadioButtonsTheme
+import com.qualtrics.digital.theming.embedded.SubmitButtonTheme
+import com.qualtrics.digital.theming.embedded.ThankYouTheme
+import com.qualtrics.digital.theming.embedded.response.EmojiTheme
+import com.qualtrics.digital.theming.embedded.response.StarTheme
+import com.qualtrics.digital.theming.embedded.response.TextInputTheme
+import com.qualtrics.digital.theming.embedded.response.ThumbsButtonsTheme
+import com.qualtrics.digital.theming.embedded.response.YesNoButtonsTheme
+import com.qualtrics.digital.theming.fonts.FontTheme
+import com.qualtrics.digital.theming.prompt.ButtonTheme
+import com.qualtrics.digital.theming.prompt.MobileAppPromptTheme
 import dagger.hilt.android.qualifiers.ApplicationContext
 import uk.gov.govuk.analytics.data.local.model.EcommerceEvent
 import javax.inject.Inject
@@ -23,6 +39,9 @@ class QualtricsAnalyticsClient @Inject constructor(
             BuildConfig.QUALTRICS_PROJECT_ID,
             context
         )
+
+        qualtrics.creativeTheme = qualtricsTheme()
+
         isInitialized = true
     }
 
@@ -83,5 +102,110 @@ class QualtricsAnalyticsClient @Inject constructor(
         analyticsParameterKeys.forEach { key ->
             qualtrics.properties.setString(key, parameters[key]?.toString() ?: "")
         }
+    }
+
+    private fun qualtricsTheme(): QualtricsTheme {
+        // TODO: Qualtrics SDK v3 - Remove XML color bridge and use .toArgb() directly
+        // TODO: Example: GovUkTheme.colourScheme.surfaces.background.toArgb()
+
+        // TODO: Similar for fonts...
+        val bodyRegular = R.font.transport_light
+        val bodyBold = R.font.transport_bold
+        val regularSize = 17
+        val mediumSize = 22
+        val largeSize = 28
+
+        return QualtricsTheme.Builder()
+            .setMobileAppPromptTheme(
+                MobileAppPromptTheme(
+                    backgroundColor = R.color.background,
+                    headlineTextColor = R.color.text,
+                    headlineFont = FontTheme(bodyBold, largeSize),
+                    descriptionTextColor = R.color.text,
+                    descriptionFont = FontTheme(bodyRegular, regularSize),
+                    closeButtonColor = R.color.background,
+                    closeButtonBackgroundColor = R.color.primary,
+                    buttonOneTheme = ButtonTheme(
+                        labelColor = R.color.primary,
+                        font = FontTheme(bodyRegular, mediumSize),
+                        backgroundColor = R.color.background,
+                        borderColor = R.color.primary,
+                        linkColor = R.color.link
+                    ),
+                    buttonTwoTheme = ButtonTheme(
+                        labelColor = R.color.background,
+                        font = FontTheme(bodyRegular, mediumSize),
+                        backgroundColor = R.color.primary,
+                        borderColor = R.color.primary,
+                        linkColor = R.color.link
+                    )
+                )
+            )
+            .setEmbeddedAppFeedbackTheme(
+                EmbeddedAppFeedbackTheme(
+                    dialogBackgroundColor = R.color.background,
+                    closeButtonColor = R.color.background,
+                    closeButtonBackgroundColor = R.color.primary,
+                    initialQuestionTheme = InitialQuestionTheme(
+                        color = R.color.text,
+                        initialQuestion = FontTheme(bodyBold, largeSize)
+                    ),
+                    followupQuestionTheme = FollowupQuestionTheme(
+                        color = R.color.text,
+                        followupQuestionFont = FontTheme(bodyBold, largeSize),
+                        followupQuestionTextInputFont = FontTheme(bodyRegular, regularSize)
+                    ),
+                    thankYouTheme = ThankYouTheme(
+                        color = R.color.text,
+                        thankYouTextFont = FontTheme(bodyBold, largeSize),
+                    ),
+                    yesNoButtonsTheme = YesNoButtonsTheme(
+                        yesButtonTextColor = R.color.background,
+                        yesButtonBorderColor = R.color.primary,
+                        yesButtonFillColor = R.color.primary,
+                        yesButtonFont = FontTheme(bodyRegular, mediumSize),
+                        noButtonTextColor = R.color.primary,
+                        noButtonBorderColor = R.color.primary,
+                        noButtonFillColor = R.color.background,
+                        noButtonFont = FontTheme(bodyRegular, mediumSize),
+                    ),
+                    thumbsButtonsTheme = ThumbsButtonsTheme(
+                        thumbUpBorderColor = R.color.text,
+                        thumbUpFillColor = R.color.secondary,
+                        thumbDownBorderColor = R.color.text,
+                        thumbDownFillColor = R.color.secondary
+                    ),
+                    emojiTheme = EmojiTheme(
+                        borderColor = R.color.primary,
+                        fillColor = R.color.primary,
+                        tintColor = R.color.background
+                    ),
+                    starTheme = StarTheme(
+                        borderColor = R.color.primary
+                    ),
+                    multipleChoiceTheme = MultipleChoiceTheme(
+                        questionTextFont = FontTheme(bodyRegular, regularSize),
+                        otherAnswerTextColor = R.color.background,
+                        otherAnswerTextFont = FontTheme(bodyRegular, regularSize),
+                        otherAnswerBackgroundColor = R.color.secondary,
+                        radioButtonsTheme = RadioButtonsTheme(
+                            textFont = FontTheme(bodyRegular, regularSize),
+                            selectedCircleColor = R.color.background,
+                            selectedBackgroundColor = R.color.secondary,
+                            unselectedCircleColor = R.color.secondary,
+                        )
+                    ),
+                    submitButtonTheme = SubmitButtonTheme(
+                        textColor = R.color.background,
+                        fillColor = R.color.primary,
+                        font = FontTheme(bodyRegular, mediumSize)
+                    ),
+                    textInputTheme = TextInputTheme(
+                        multilineTextInputColor = R.color.background,
+                        multilineTextInputBackgroundColor = R.color.secondary,
+                    )
+                )
+            )
+            .build()
     }
 }
