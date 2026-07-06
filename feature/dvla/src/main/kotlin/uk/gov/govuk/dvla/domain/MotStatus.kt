265de392a29b8525ad998d8e51797258f6361997
TimDenyer <timgjdenyer@gmail.com>
2026-07-06 14:54:18 +0100

    Fix formatting

--- feature/dvla/src/main/kotlin/uk/gov/govuk/dvla/domain/MotStatus.kt
+++ feature/dvla/src/main/kotlin/uk/gov/govuk/dvla/domain/MotStatus.kt
@@ -2,7 +2,6 @@ package uk.gov.govuk.dvla.domain
 
 import uk.gov.govuk.dvla.remote.model.common.MotStatus as RemoteMotStatus
 
-
 enum class MotStatus {
     VALID,
     EXPIRED,
@@ -17,7 +16,7 @@ internal fun RemoteMotStatus?.toDomain(): MotStatus {
     return when (this) {
         RemoteMotStatus.VALID -> MotStatus.VALID
         RemoteMotStatus.NOT_VALID -> MotStatus.EXPIRED
-        RemoteMotStatus.NO_DETAILS_HELD-> MotStatus.NO_DETAILS_HELD
+        RemoteMotStatus.NO_DETAILS_HELD -> MotStatus.NO_DETAILS_HELD
         RemoteMotStatus.NO_RESULTS_RETURNED -> MotStatus.NO_RESULTS_RETURNED
     }
 }
