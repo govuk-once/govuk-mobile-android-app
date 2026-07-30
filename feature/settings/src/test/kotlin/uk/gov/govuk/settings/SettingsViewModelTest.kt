1e13c36db0954e545d2711848504138db46f6375
Matt Malone <matthew.malone@digital.cabinet-office.gov.uk>
2026-07-30 17:33:29 +0100

    fix: message count on Settings not updating

--- feature/settings/src/test/kotlin/uk/gov/govuk/settings/SettingsViewModelTest.kt
+++ feature/settings/src/test/kotlin/uk/gov/govuk/settings/SettingsViewModelTest.kt
@@ -316,7 +316,7 @@ class SettingsViewModelTest {
         runTest {
             every { dvlaRepo.linkState } returns flowOf(ServiceLinkStatus.CHECKING)
 
-            viewModel.onPageView()
+            viewModel.loadMessages()
 
             advanceUntilIdle()
 
@@ -330,7 +330,7 @@ class SettingsViewModelTest {
         runTest {
             every { dvlaRepo.linkState } returns flowOf(ServiceLinkStatus.UNLINKED)
 
-            viewModel.onPageView()
+            viewModel.loadMessages()
 
             advanceUntilIdle()
 
@@ -343,7 +343,7 @@ class SettingsViewModelTest {
         runTest {
             every { dvlaRepo.linkState } returns flowOf(ServiceLinkStatus.ERROR)
 
-            viewModel.onPageView()
+            viewModel.loadMessages()
 
             advanceUntilIdle()
 
@@ -358,7 +358,7 @@ class SettingsViewModelTest {
             every { dvlaRepo.linkState } returns flowOf(ServiceLinkStatus.LINKED)
             coEvery { notificationCentreFeature.getUnreadCount() } returns null
 
-            viewModel.onPageView()
+            viewModel.loadMessages()
 
             advanceUntilIdle()
 
@@ -372,7 +372,7 @@ class SettingsViewModelTest {
             every { dvlaRepo.linkState } returns flowOf(ServiceLinkStatus.LINKED)
             coEvery { notificationCentreFeature.getUnreadCount() } returns 1
 
-            viewModel.onPageView()
+            viewModel.loadMessages()
 
             advanceUntilIdle()
 
@@ -390,13 +390,36 @@ class SettingsViewModelTest {
             }
             coEvery { notificationCentreFeature.getUnreadCount() } returns 3
 
-            viewModel.onPageView()
+            viewModel.loadMessages()
 
             assertEquals(MessageRowState.Loading, viewModel.uiState.value?.messageRowState)
 
             advanceUntilIdle()
 
             assertEquals(MessageRowState.Loaded(3), viewModel.uiState.value?.messageRowState)
+        }
+    }
+
+    @Test
+    fun `Given loadMessages is called again, the previous DVLA link collector is cancelled`() {
+        runTest {
+            every { dvlaRepo.linkState } returns flow {
+                emit(ServiceLinkStatus.LINKED)
+                delay(1000)
+                emit(ServiceLinkStatus.ERROR)
+            }
+            coEvery { notificationCentreFeature.getUnreadCount() } returns 5
+
+            viewModel.loadMessages()
+
+            every { dvlaRepo.linkState } returns flowOf(ServiceLinkStatus.LINKED)
+            coEvery { notificationCentreFeature.getUnreadCount() } returns 7
+
+            viewModel.loadMessages()
+
+            advanceUntilIdle()
+
+            assertEquals(MessageRowState.Loaded(7), viewModel.uiState.value?.messageRowState)
         }
     }
 }
