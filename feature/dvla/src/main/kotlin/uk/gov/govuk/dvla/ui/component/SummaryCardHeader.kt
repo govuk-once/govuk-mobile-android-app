c1c61f77336ac7e9c14d82ebfcc21c264bcbe1d7
lamprosgk <lampros.gkotsoulias@digital.cabinet-office.gov.uk>
2026-08-05 10:15:49 +0100

    GOVUKAPP-3822 Close menu navigation  (#803)
    * Added 'Close menu' option in summary card when TB on
    
    * Added menu item divider for close

--- feature/dvla/src/main/kotlin/uk/gov/govuk/dvla/ui/component/SummaryCardHeader.kt
+++ feature/dvla/src/main/kotlin/uk/gov/govuk/dvla/ui/component/SummaryCardHeader.kt
@@ -1,5 +1,7 @@
 package uk.gov.govuk.dvla.ui.component
 
+import android.content.Context
+import android.view.accessibility.AccessibilityManager
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,25 +15,52 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MenuDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import uk.gov.govuk.design.ui.component.BodyRegularLabel
 import uk.gov.govuk.design.ui.component.CardListItem
 import uk.gov.govuk.design.ui.component.OverflowButton
+import uk.gov.govuk.design.ui.extension.withAltText
+import uk.gov.govuk.design.ui.model.AccessibleString
 import uk.gov.govuk.design.ui.theme.GovUkTheme
 import uk.gov.govuk.dvla.R
 import uk.gov.govuk.dvla.ui.model.OverflowMenuItem
+
+@Composable
+private fun isTalkBackEnabled(): Boolean {
+    val context = LocalContext.current
+    val accessibilityManager =
+        context.getSystemService(Context.ACCESSIBILITY_SERVICE) as AccessibilityManager
+
+    var isEnabled by remember {
+        mutableStateOf(accessibilityManager.isTouchExplorationEnabled)
+    }
+
+    DisposableEffect(accessibilityManager) {
+        val listener = AccessibilityManager.TouchExplorationStateChangeListener { enabled ->
+            isEnabled = enabled
+        }
+        accessibilityManager.addTouchExplorationStateChangeListener(listener)
+
+        onDispose {
+            accessibilityManager.removeTouchExplorationStateChangeListener(listener)
+        }
+    }
+
+    return isEnabled
+}
 
 @Composable
 internal fun SummaryCardHeader(
@@ -78,6 +107,7 @@ private fun CardOverflowMenu(
     onMenuItemClick: (OverflowMenuItem) -> Unit
 ) {
     var expanded by remember { mutableStateOf(false) }
+    val isTalkBackOn = isTalkBackEnabled()
 
     Box {
         OverflowButton(
@@ -93,29 +123,46 @@ private fun CardOverflowMenu(
             offset = DpOffset(x = 0.dp, y = GovUkTheme.spacing.extraSmall)
         ) {
             menuItems.forEach { item ->
-                DropdownMenuItem(
-                    text = {
-                        BodyRegularLabel(
-                            text = item.text.displayText,
-                            color = GovUkTheme.colourScheme.textAndIcons.primary,
-                            modifier = item.text.altText?.let {
-                                Modifier.semantics { contentDescription = it }
-                            } ?: Modifier
-                        )
-                    },
+                OverflowMenuItemRow(
+                    title = AccessibleString(item.text.displayText, item.text.altText),
                     onClick = {
                         onMenuItemClick(item)
                         expanded = false
-                    },
-                    contentPadding = PaddingValues(
-                        horizontal = GovUkTheme.spacing.medium,
-                        vertical = GovUkTheme.spacing.small
-                    ),
-                    colors = MenuDefaults.itemColors(
-                        textColor = GovUkTheme.colourScheme.textAndIcons.primary
-                    )
+                    }
+                )
+            }
+
+            if (isTalkBackOn) {
+                HorizontalDivider()
+                OverflowMenuItemRow(
+                    title = AccessibleString(stringResource(R.string.menu_close_menu)),
+                    onClick = { expanded = false }
                 )
             }
         }
     }
+}
+
+@Composable
+private fun OverflowMenuItemRow(
+    title: AccessibleString,
+    onClick: () -> Unit
+) {
+    DropdownMenuItem(
+        text = {
+            BodyRegularLabel(
+                text = title.displayText,
+                color = GovUkTheme.colourScheme.textAndIcons.primary,
+                modifier = Modifier.withAltText(title.altText)
+            )
+        },
+        onClick = onClick,
+        contentPadding = PaddingValues(
+            horizontal = GovUkTheme.spacing.medium,
+            vertical = GovUkTheme.spacing.small
+        ),
+        colors = MenuDefaults.itemColors(
+            textColor = GovUkTheme.colourScheme.textAndIcons.primary
+        )
+    )
 }
