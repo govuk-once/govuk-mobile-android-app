dd5215b1d9dc0b6943fd6a6424ed5d45bda37dcf
lamprosgk <lampros.gkotsoulias@digital.cabinet-office.gov.uk>
2026-08-05 12:18:36 +0100

    GOVUKAPP-3870 Remove logging interceptor lib (#810)
    Removing logging interceptor lib

--- data/src/main/kotlin/uk/gov/govuk/data/di/NetworkModule.kt
+++ data/src/main/kotlin/uk/gov/govuk/data/di/NetworkModule.kt
@@ -7,7 +7,6 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import uk.gov.govuk.data.BuildConfig
