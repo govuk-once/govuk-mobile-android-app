64d8dfe9cdbcf2f34d6f7d08d841b27228fea3d8
Lampros.Gkotsoulias <lampros.gkotsoulias@digital.cabinet-office.gov.uk>
2026-08-06 15:10:30 +0100

    Increased test coverage

--- feature/dvla/src/test/java/uk/gov/govuk/dvla/VehiclesAndLicenceSummaryViewModelTest.kt
+++ feature/dvla/src/test/java/uk/gov/govuk/dvla/VehiclesAndLicenceSummaryViewModelTest.kt
@@ -33,6 +33,7 @@ import uk.gov.govuk.dvla.ui.model.UrlModel
 import uk.gov.govuk.dvla.ui.model.VehicleSummaryMapper
 import uk.gov.govuk.dvla.ui.model.VehicleSummaryUiModel
 import uk.gov.govuk.dvla.ui.model.VehiclesSummaryUiState
+import uk.gov.govuk.govkit.browser.Urls
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class VehiclesAndLicenceSummaryViewModelTest {
@@ -146,7 +147,7 @@ class VehiclesAndLicenceSummaryViewModelTest {
             every { dvlaRepo.linkState } returns MutableStateFlow(ServiceLinkStatus.LINKED)
             coEvery { dvlaRepo.getCustomerVehicles() } returns Result.Error()
             coEvery { dvlaRepo.getLicenceDetails() } returns LicenceDetailsResult.Failure(Result.Error())
-            every { configRepo.dvlaUrls?.account } returns null
+            every { configRepo.dvlaUrls } returns null
 
             val viewModel = VehiclesAndLicenceSummaryViewModel(
                 dvlaRepo,
@@ -161,7 +162,7 @@ class VehiclesAndLicenceSummaryViewModelTest {
             coVerify(exactly = 1) { dvlaRepo.getLicenceDetails() }
 
             val currentState = viewModel.uiState.value as UiState.Default
-            assertEquals(VehiclesSummaryUiState.Error(UrlModel("https://www.gov.uk")), currentState.vehiclesState)
+            assertEquals(LicenceSummaryUiState.Error(UrlModel(Urls.GOV_UK_HOME)), currentState.licenceState)
         }
 
     @Test
@@ -188,7 +189,7 @@ class VehiclesAndLicenceSummaryViewModelTest {
     fun `Given linkState emits ERROR, when viewModel initialised and dvla urls doesn't have account, then state is Error and no calls are made`() =
         runTest(dispatcher) {
             every { dvlaRepo.linkState } returns MutableStateFlow(ServiceLinkStatus.ERROR)
-            every { configRepo.dvlaUrls?.account } returns null
+            every { configRepo.dvlaUrls } returns null
 
             val viewModel = VehiclesAndLicenceSummaryViewModel(
                 dvlaRepo,
@@ -199,7 +200,7 @@ class VehiclesAndLicenceSummaryViewModelTest {
             )
             advanceUntilIdle()
 
-            assertEquals(UiState.Error(UrlModel("https://www.gov.uk")), viewModel.uiState.value)
+            assertEquals(UiState.Error(UrlModel(Urls.GOV_UK_HOME)), viewModel.uiState.value)
             coVerify(exactly = 0) { dvlaRepo.getCustomerVehicles() }
             coVerify(exactly = 0) { dvlaRepo.getLicenceDetails() }
         }
