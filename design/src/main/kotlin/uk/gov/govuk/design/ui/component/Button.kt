27f1105ae6f836f32f1628d2533a5985e1fa1f6d
Lampros.Gkotsoulias <lampros.gkotsoulias@digital.cabinet-office.gov.uk>
2026-08-13 13:23:19 +0100

    Remove unused import

--- design/src/main/kotlin/uk/gov/govuk/design/ui/component/Button.kt
+++ design/src/main/kotlin/uk/gov/govuk/design/ui/component/Button.kt
@@ -19,13 +19,11 @@ import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
