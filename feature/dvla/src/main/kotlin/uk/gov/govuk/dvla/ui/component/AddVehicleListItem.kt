8ed8b7433e87ba39d1dd8bfd9e3c04c6a06d288a
Lampros.Gkotsoulias <lampros.gkotsoulias@digital.cabinet-office.gov.uk>
2026-09-02 13:56:43 +0100

    Focus indicator for cards

--- feature/dvla/src/main/kotlin/uk/gov/govuk/dvla/ui/component/AddVehicleListItem.kt
+++ feature/dvla/src/main/kotlin/uk/gov/govuk/dvla/ui/component/AddVehicleListItem.kt
@@ -1,21 +1,47 @@
 package uk.gov.govuk.dvla.ui.component
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
-import androidx.compose.ui.unit.dp
 import uk.gov.govuk.design.ui.component.BodyRegularLabel
 import uk.gov.govuk.design.ui.component.CardListItem
 import uk.gov.govuk.design.ui.component.SmallHorizontalSpacer
 import uk.gov.govuk.design.ui.theme.GovUkTheme
 
+private data class AddVehicleListItemColours(
+    val background: Color,
+    val text: Color,
+    val icon: Color
+)
+
+@Composable
+private fun resolveAddVehicleListItemColours(isFocused: Boolean): AddVehicleListItemColours {
+    return if (isFocused) {
+        AddVehicleListItemColours(
+            background = GovUkTheme.colourScheme.surfaces.focused,
+            text = GovUkTheme.colourScheme.textAndIcons.focused,
+            icon = GovUkTheme.colourScheme.textAndIcons.focused
+        )
+    } else {
+        AddVehicleListItemColours(
+            background = GovUkTheme.colourScheme.surfaces.list,
+            text = GovUkTheme.colourScheme.textAndIcons.primary,
+            icon = GovUkTheme.colourScheme.textAndIcons.linkPrimary
+        )
+    }
+}
 @Composable
 fun AddVehicleListItem(
     title: String,
@@ -25,12 +51,19 @@ fun AddVehicleListItem(
     isFirst: Boolean = true,
     isLast: Boolean = true
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocused by interactionSource.collectIsFocusedAsState()
+
+    val colours = resolveAddVehicleListItemColours(isFocused = isFocused)
+
     CardListItem(
         modifier = modifier,
         onClick = onClick,
+        interactionSource = interactionSource,
         isFirst = isFirst,
         isLast = isLast,
-        drawDivider = true
+        drawDivider = true,
+        background = colours.background
     ) {
         Row(
             modifier = Modifier
@@ -41,7 +74,7 @@ fun AddVehicleListItem(
             BodyRegularLabel(
                 text = title,
                 modifier = Modifier.weight(1f),
-                color = GovUkTheme.colourScheme.textAndIcons.primary
+                color = colours.text
             )
 
             SmallHorizontalSpacer()
@@ -49,7 +82,7 @@ fun AddVehicleListItem(
             Icon(
                 painter = painterResource(id = icon),
                 contentDescription = null,
-                tint = GovUkTheme.colourScheme.textAndIcons.linkPrimary
+                tint = colours.icon
             )
         }
     }
